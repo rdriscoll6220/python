7cc73f81e9d1ac9249ac1a8acd2dfb652c4e114c
Ron Driscoll <rdriscoll6220@gmail.com>
2021-11-15 16:21:05 -0800

    Update

---
+++ letsgo.py
@@ -0,0 +1,33 @@
+from time import sleep
+
+print("This is my file to demonstrate best practices.")
+
+def centralNervousSystem():
+    print("Setting up Central Nervous System...")
+    print("\tConfirming connectivity to all graphs...")
+    print("\tChecking graph health...")
+
+def peripheralNervousSystem():
+    print("Setting up Peripheral Nervous System...")
+
+def autonomicNervousSystem():
+    print("Setting up Autonomic Nervous System...")
+
+def somaticNervousSystem():
+    print("Setting up Somatic Nervous System...")
+
+
+def main():
+ 
+    centralNervousSystem()
+    sleep(3)
+    peripheralNervousSystem()
+    sleep(3)
+    autonomicNervousSystem()
+    sleep(3)
+    somaticNervousSystem()
+    sleep(3)
+    
+if __name__ == "__main__":
+    main()
+
